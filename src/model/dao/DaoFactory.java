1a791a291d7eac8517321222b7ae50690ce83423
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-07-26 15:33:15 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaojDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao CreateSellerDao() {
-		return new SellerDaojDBC();
+		return new SellerDaojDBC(DB.getConnection());
 	}
 
 }
